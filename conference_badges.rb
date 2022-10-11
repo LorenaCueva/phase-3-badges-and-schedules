d7fa93e59885aedfb8a3f0c464ca9e6ed1a9c045
Lorena Cueva <lorenacueva@gmail.com>
2022-10-10 17:34:23 -0700

    commit tree e2e298cc37e8c5081ed319c525a9bd0f9efbd883

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    arr.map{|speaker| badge_maker(speaker)}
+end
+
+def assign_rooms(arr)
+    arr.map.with_index(1) do |speaker, room|
+        "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(arr)
+    batch_badge_creator(arr).each {|badge| puts badge}
+    assign_rooms(arr).each {|assigned_room| puts assigned_room}
+end
+
